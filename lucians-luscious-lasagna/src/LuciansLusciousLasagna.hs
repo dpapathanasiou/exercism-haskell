44db72aa7d40fb03a83f9f7f470b112fce1e9629
Denis Papathanasiou <denis@papathanasiou.org>
2022-09-18 18:04:25 -0400

    Second exercise: Lucian's Luscious Lasagna (simple functions)

---
+++ lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
@@ -0,0 +1,10 @@
+module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
+
+expectedMinutesInOven :: Integer
+expectedMinutesInOven = 40
+
+preparationTimeInMinutes :: Integer -> Integer
+preparationTimeInMinutes layers = layers * 2
+
+elapsedTimeInMinutes :: Integer -> Integer -> Integer
+elapsedTimeInMinutes layers timeAlreadyInOven = preparationTimeInMinutes layers + timeAlreadyInOven
