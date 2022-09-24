f2b2d41deb765aa56de3af471f3d9ecdd83491c9
Denis Papathanasiou <denis@papathanasiou.org>
2022-09-24 16:25:39 -0400

    Exercise 11: using Data.Map

---
+++ nucleotide-count/src/DNA.hs
@@ -0,0 +1,24 @@
+module DNA (nucleotideCounts, Nucleotide(..)) where
+
+import Data.Map (fromList, insertWith, notMember, Map)
+
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+
+toNucleotide :: Char -> Nucleotide
+toNucleotide c
+  | c == 'A' = A
+  | c == 'C' = C
+  | c == 'G' = G
+  | otherwise = T
+
+foo :: String -> Map Nucleotide Int -> Either String (Map Nucleotide Int)
+foo [] m = Right m
+foo (x:xs) m
+  | x `notElem` ['A','C','G','T'] = Left "Error"
+  | notMember nucleotide m = Left "Error"
+  | otherwise = foo xs (insertWith (+) nucleotide 1 m)
+  where
+      nucleotide = toNucleotide x
+
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts xs = foo xs (fromList[(A,0), (C,0), (G,0), (T, 0)])
