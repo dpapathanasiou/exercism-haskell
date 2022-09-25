35066b806ea62b9251baebd9692c9e901727bf73
Denis Papathanasiou <denis@papathanasiou.org>
2022-09-25 11:11:24 -0400

    Exercise 10: take 3, even better using traverse (example from https://exercism.org/tracks/haskell/exercises/rna-transcription/solutions/phuongduyphan)

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -7,11 +7,5 @@ dnaToRna 'T' = Right 'A'
 dnaToRna 'A' = Right 'U'
 dnaToRna  x  = Left x
 
-foo :: String -> String -> Either Char String
-foo [] a = Right a
-foo (x:xs) a = case dnaToRna x of
-    Right rna -> foo xs (a ++ [rna])
-    Left  err -> Left err
-
 toRNA :: String -> Either Char String
-toRNA text = foo text ""
+toRNA = traverse dnaToRna
