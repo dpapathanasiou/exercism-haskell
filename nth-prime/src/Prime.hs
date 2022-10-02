3ca921892c1fb43ffa817a1a8653cfe1fa49519d
Denis Papathanasiou <denis@papathanasiou.org>
2022-10-02 18:40:29 -0400

    Exercise 20: take 2, using lazy evaluation on infinite lists more idiomatically

--- nth-prime/src/Prime.hs
+++ nth-prime/src/Prime.hs
@@ -1,15 +1,12 @@
 module Prime (nth) where
 
-sieve :: Integer -> [Integer]
-sieve n = [toInteger x | x <- [2 .. n], and [x `mod` y /= 0 | y <- [2 .. (floor (sqrt (fromInteger x)))]]]
-
-findNth :: Int -> Integer -> Integer
-findNth target index
-  | length primes >= target = last (take target primes)
-  | otherwise = findNth target (index * 100) -- sieve is efficient, so jump an arbitrary large amount
-  where primes = sieve index
+primes :: [Integer]
+primes = sieve [2..]
+  where
+    sieve [] = []
+    sieve (x:xs) = x : sieve [p | p <- xs, mod p x /= 0]
 
 nth :: Int -> Maybe Integer
 nth n
-  | n < 1 = Nothing
-  | otherwise = Just (findNth n 2)
+  | n > 0 = Just (primes !! (n - 1))
+  | otherwise = Nothing
