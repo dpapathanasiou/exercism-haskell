b29b045c77ef853f84818851252b6a25fe5f4873
Denis Papathanasiou <denis@papathanasiou.org>
2022-10-10 19:19:05 -0400

    Exercise 26 (take 2): pascal's triangle as an infinite list of lists

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,13 +1,7 @@
 module Triangle (rows) where
 
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n = foldr (*) 1 [1 .. n]
-
-binomial :: Int -> Int -> Int
-binomial n k = factorial n `div` (factorial k * factorial (n - k))
-
--- The entry in the nth row and kth column of Pascal's triangle is denoted: binomial n k
+-- generate an infinite list of lists and take the requested amount
 
 rows :: Int -> [[Integer]]
-rows x = [[toInteger (binomial r c) | c <- [0 .. r]] | r <- [0 .. (x - 1)]]
+rows x = take x $ iterate nextRow [1]
+    where nextRow priorRow = zipWith (+) (0 : priorRow) (priorRow ++ [0])
